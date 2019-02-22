0facb9dcf5566faa7224bcc6bbeb0fd9a1cd794b
Vogan <jbdedofus@hotmail.fr>
2019-02-22 15:09:55 +0100

    Level 1 a test

---
+++ MakeYourOwnEscape/Assets/Scripts/OnGroundScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGroundScript : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Player"))
+        {
+            //restart
+            Debug.Log("restart");
+        }
+    }
+}
