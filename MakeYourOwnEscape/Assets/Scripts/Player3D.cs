5f12609850f4de3a08d8b7ab8aebffeb8e07294f
Dagaubs <alan181096@gmail.com>
2019-02-22 17:28:46 +0100

    Movement Player FINALLY WOOOOOOORKS ! im happy

--- MakeYourOwnEscape/Assets/Scripts/Player3D.cs
+++ MakeYourOwnEscape/Assets/Scripts/Player3D.cs
@@ -59,64 +59,37 @@ public class Player3D : MonoBehaviour {
 			//Debug.Log("direction_joystick : " + direction_joystick);
 			if(direction_joystick.magnitude > 0.1f && mouseDown){
 				Vector3 Z_prime = Camera.main.cameraToWorldMatrix.MultiplyVector(Vector3.zero);
-				Vector3 axis = Vector3.Cross(transform.up, Z_prime);
-				float angle = Vector3.Angle(transform.up, Z_prime);
+				//Vector3 axis = Vector3.Cross(transform.up, Z_prime);
+				//float angle = Vector3.Angle(transform.up, Z_prime);
                 //transform.RotateAround(transform.position, axis, angle);
-
-                Debug.DrawRay(transform.position, transform.forward, Color.green);
+                
                 //Vector3 xy_p = Camera.main.cameraToWorldMatrix.MultiplyVector(direction_joystick);
                 Vector3 xy_p = direction_joystick;
-                //float angletest = Vector3.Angle(transform.forward, xy_p - Z_prime);
 
-
-                //transform.RotateAround(transform.position, axis, -angle);
                 float angle2 = Vector3.Angle(transform.forward, xy_p - Z_prime);
-                //Debug.Log("angletest : " + angletest + " | angle2 : " + angle2);
 
                 Debug.DrawRay(Z_prime, xy_p - Z_prime, Color.gray);
                 Debug.DrawRay(Z_prime, transform.forward, Color.cyan);
-                /*
-                var line = transform.position + (transform.forward * visionDistance);
-                var rotatedLine = Quaternion.AngleAxis(angle2, transform.up) * line;
-                Debug.DrawLine(transform.position, rotatedLine, Color.cyan);*/
 
-                //Debug.Break();
-                Debug.DrawRay(transform.position, transform.forward, Color.red);
                 Debug.DrawLine(Camera.main.transform.position, Z_prime, Color.yellow);
                 Debug.DrawLine(Camera.main.transform.position, xy_p, Color.blue);
 
                 //Vector3 actuallyLookingPoint = Vector3.zero - transform.forward + Z_prime;
-                Vector3 direction = xy_p - transform.forward;
-                Debug.Log("actually : " + transform.forward + " dir : " + direction);
-
-                if((direction.x < 0 && direction.z < 0) || (direction.x > 0 && direction.z > 0)) // needs to set reverse angle !
-                {
-                    angle2 = 2 * angle2 / 5;
-                }
-                else
+                //Vector3 direction = xy_p - transform.forward;
+                //Debug.Log("actually : " + transform.forward + " dir : " + xy_p);
+                bool _isLeft = isLeft(-transform.forward, transform.forward, xy_p);
+                if (_isLeft)
                 {
                     angle2 = -2 * angle2 / 5;
                 }
+                else
+                {
+                    angle2 = 2 * angle2 / 5;
+                }
 
-                Debug.Log("angle 2 : " + angle2);
+                //Debug.Log("isLeft : " + _isLeft + " | angle 2 : " + angle2 + " | xy_p : " + xy_p);
                 if(_break)
                     Debug.Break();
-                /*if (angle2 > step)
-					Debug.Log("Angle above " + step + " : " + angle2);
-				else if(angle2 < 0f)
-					Debug.Log("NEGATIVE ANGLE : " + angle2);
-	
-				if(angle2 > step){
-					float newAngle = angle2 - removeStep;
-					//Debug.Log("Old Angle : " + angle2 + " | new Angle : " + newAngle);
-					angle2 = newAngle;
-				}
-				//angle2 = angle2 > 90f ? angle2 - 180f: angle2;
-                */
-
-                /* float rotatedAngle = angle2 > 90 ? 90 - angle2 : angle2;
-
-                 Debug.Log("angle2 : " + angle2 + " | rotated: " + rotatedAngle);*/
 
                 transform.RotateAround(transform.position, transform.up, angle2);
 
@@ -173,4 +146,9 @@ public class Player3D : MonoBehaviour {
  
 		velocity = velocity / 3;
 	}
+
+    public bool isLeft(Vector3 a, Vector3 b, Vector3 c)
+    {
+        return ((b.x - a.x) * (c.z - a.z) - (b.z - a.z) * (c.x - a.x)) > 0;
+    }
 }
